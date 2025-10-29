13f23e95bd8151da7b030a6b7effcf193a95957d
Bruno Soares <brunosilvsoares@gmail.com>
2025-10-29 16:18:31 -0500

    Completed the proposed activity for inputs and outputs. Added a few more lines to practice a bit more

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        Console.Write($"How old are you {first_name}? ");
+        string number = Console.ReadLine();
+        int age = int.Parse(number);
+        int years = 100 - age;
+        Console.Write($"{first_name}, it will take you {years} years to be 100 years old!");
     }
 }
