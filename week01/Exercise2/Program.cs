132e79c7a066ee728f841d243f066783ef954259
Bruno Soares <brunosilvsoares@gmail.com>
2025-10-29 17:07:00 -0500

    wrote the code according to activity specifications

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.Write("Enter your grade: ");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+        if (grade >= 90)
+        {
+            Console.Write("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.Write("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.Write("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("F");
+        }
+    }   
 }
