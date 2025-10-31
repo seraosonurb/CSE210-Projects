7f993af2e453c1dd4ad24f0665930ab2822211fa
Bruno Soares <brunosilvsoares@gmail.com>
2025-10-31 05:35:15 -0500

    Reviewed the exercises and completed the strech challenges of exercise 3

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,15 +6,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        string first = Console.ReadLine();
 
-        Console.Write($"How old are you {first_name}? ");
-        string number = Console.ReadLine();
-        int age = int.Parse(number);
-        int years = 100 - age;
-        Console.Write($"{first_name}, it will take you {years} years to be 100 years old!");
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.Write($"Your name is {last}, {first} {last}.");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,30 +1,43 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+        Random randomGenerador = new Random();
+        int magicNumber = randomGenerador.Next(1, 100);
         int guess = -1;
-
-        while (guess != magicNumber)
+        int count = 0;
+        string response = "";
+        do
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            magicNumber = randomGenerador.Next(1, 100);
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    count += 1;
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higer");
+                    count += 1;
+                }
+                else
+                {
+                    count += 1;
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {count} times to guess.");
+                }
+            } while (guess != magicNumber);
 
-            if (magicNumber > guess)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+
+        } while (response != "no");
     }
 }
