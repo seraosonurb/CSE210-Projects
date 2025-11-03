afe7ab80f087b5ae0034f386c1065b7607eb7fd7
Bruno Soares <brunosilvsoares@gmail.com>
2025-11-03 17:53:07 -0500

    Program running according to requirements and instructions

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Runtime.Serialization.Json;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engeneer";
+        job1._company = "Microsoft";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data analist";
+        job2._company = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+
+        Resume myResume = new Resume();
+        myResume._newJob.Add(job1);
+        myResume._newJob.Add(job2);
+
+        myResume._name = "Allison Rose";
+        myResume.DisplayResume();
+
     }
 }
---
+++ week02/Resumes/Resumes.cs
@@ -0,0 +1,16 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _newJob = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name} ");
+        Console.WriteLine("Jobs: ");
+        foreach (Job job in _newJob)
+        {
+            job.Display();
+        }
+    }
+
+}
